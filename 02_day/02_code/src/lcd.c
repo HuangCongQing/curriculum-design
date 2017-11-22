8fe97fa0c65287fca19777c14c0b7ced4485ba8a
HuangChongqing <1756260160@qq.com>
2017-11-22 23:15:52 +0800

    function:lcd_rgb_mmap3(error)

--- 02_day/02_code/src/lcd.c
+++ 02_day/02_code/src/lcd.c
@@ -14,11 +14,12 @@ int lcd_rgb(unsigned int _argb){
 	}
 
 	unsigned int buff[800*480] = {0};
-	//3. 构造颜色信息
+	// 3. 构造颜色信息
 	for (int i = 0; i < 800*480; ++i)
 	{
 		buff[i] = _argb;
 	}
+	
 
 	ssize_t ret = write(lcd_fd, buff, 800*480*4);
 	if(-1 == ret){
@@ -31,7 +32,30 @@ int lcd_rgb(unsigned int _argb){
 }
 
 
-int lcd_rgb_mmap(unsigned int _argb){
+// int lcd_rgb_mmap(unsigned int _argb){
+// 	//1. 打开lcd的设备文件 /dev/fb0  open();
+// 	int lcd_fd = open("/dev/fb0",O_RDWR);
+// 	if (-1 == lcd_fd)
+// 	{
+// 		printf("open fb0 error!\n");
+// 		return -1;
+// 	}
+
+// 	//2. 内存映射
+// 	unsigned int *lcd_buff = (unsigned int *)mmap(NULL,800*480*4, PROT_READ|PROT_WRITE,  MAP_SHARED,lcd_fd,0);
+// 	//3. 构造颜色信息
+// 	for (int i = 0; i < 800*480; ++i)
+// 	{
+// 		lcd_buff[i] = _argb;
+// 	}
+
+// 	munmap(lcd_buff,800*480*4);
+// 	close(lcd_fd);
+// 	return 0;
+// }
+
+//显示3行颜色
+int lcd_rgb_mmap3(unsigned int _argb, unsigned int _argb1, unsigned int _argb2){
 	//1. 打开lcd的设备文件 /dev/fb0  open();
 	int lcd_fd = open("/dev/fb0",O_RDWR);
 	if (-1 == lcd_fd)
@@ -43,14 +67,24 @@ int lcd_rgb_mmap(unsigned int _argb){
 	//2. 内存映射
 	unsigned int *lcd_buff = (unsigned int *)mmap(NULL,800*480*4, PROT_READ|PROT_WRITE,  MAP_SHARED,lcd_fd,0);
 	//3. 构造颜色信息
-	for (int i = 0; i < 800*480; ++i)
-	{
-		lcd_buff[i] = _argb;
-	}
+	for (int i = 0; i < 800*160; ++i)
+		{
+			buff[i] = _argb;
+		}
+		for (int i = 800*160; i < 800*320; ++i)
+		{
+			buff[i] = _argb1;
+		}
+		for (int i = 800*320; i < 800*480; ++i)
+		{
+			buff[i] = _argb2;
+		}
 
 	munmap(lcd_buff,800*480*4);
 	close(lcd_fd);
 	return 0;
 }
+
+
 
 
--- 02_day/02_code/src/main.c
+++ 02_day/02_code/src/main.c
@@ -14,15 +14,17 @@
 int main(int argc, char const *argv[])
 {
 	// 一种方法   sleep(),linux函数，默认就有
-	while(1){
-		lcd_rgb_mmap(0x0000ff);
-		sleep(1);
-		lcd_rgb_mmap(0x00ff00);
-		sleep(1);
-		lcd_rgb_mmap(0xff0000);
-		sleep(1);
-	}
+	// while(1){
+	// 	lcd_rgb_mmap(0x0000ff);
+	// 	sleep(1);
+	// 	lcd_rgb_mmap(0x00ff00);
+	// 	sleep(1);
+	// 	lcd_rgb_mmap(0xff0000);
+	// 	sleep(1);
+	// }
+
+	// 直接显示三行颜色
 	
-	
+	lcd_rgb_mmap3(0x0000ff,0x00ff00,(0xff0000);
 }
 
