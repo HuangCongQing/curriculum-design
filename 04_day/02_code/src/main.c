01a8b629f734dad9194b0d5c01e5d326fbb66dd0
HuangChongqing <1756260160@qq.com>
2017-11-25 22:48:58 +0800

    点击触摸屏-切换界面

--- 04_day/02_code/src/main.c
+++ 04_day/02_code/src/main.c
@@ -6,8 +6,7 @@ int main(int argc, char const *argv[])
 {
 	lcd_rgb_mmap(0x00ff);
 	bmp_800_480("01.bmp");
-	show_bmp_not_800_480("./02.bmp",0,0);
-	show_bmp_not_800_480("./02.bmp",111,172);
+	show_bmp_not_800_480("./02.bmp",400,240);
 	test();
 	printf("cqupt\n");
 	return 0;
---
+++ 04_day_teacher/02_code/src/ts.c
@@ -0,0 +1,31 @@
+#include "ts.h"
+
+static struct tsdev * ts_dev = NULL;
+int init_ts(){
+	//struct tsdev *ts_open(const char *dev_name, int nonblock);
+	ts_dev = ts_open("/dev/event0",0);
+	ts_config(ts_dev);
+	return 0;
+}
+
+int read_ts(struct ts_sample  *ts_p){
+	if(ts_dev == NULL){
+		init_ts();
+	}
+	printf("111\n");
+
+	int ret = ts_read(ts_dev, ts_p,1);
+	printf("tttt\n");
+
+	printf("x=%d,y=%d,p=%d\n", ts_p->x,ts_p->y,ts_p->pressure);
+	return ret;
+}
+
+int close_ts(){
+	ts_close(ts_dev);
+	ts_dev = NULL;
+	return 0;
+}
+	
+	
+
